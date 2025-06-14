b3e4aabedbb70d88cb607cf3eb02ab29747ed8c4
arjmarty <annrose0227@gmail.com>
2025-06-14 20:18:37 +0800

    modify data extraction scripts

--- data-etl.py
+++ data-etl.py
@@ -7,17 +7,34 @@ import requests
 import json
 from bs4 import BeautifulSoup
 from urllib import request
+import time
 
 
 # url of intended website
-page_url = "https://www.booking.com/"
+page_url = "https://finance.yahoo.com/markets/stocks/most-active/"
+
+def get_url(page_url):
+    for attempt in range(5):
+        response = requests.get(page_url)
+
+        if response.status_code == 200:
+            return BeautifulSoup(response.content, 'html.parser')
+        elif response == 429:
+            wait_time = 2 ** attempt
+            print(f"Received 429 error. Waiting for {wait_time} seconds before retrying...")
+            time.sleep(wait_time)
+        else:
+            print(f"Error: {response.status_code}")
+            break
+    
+    return None
+
+
 
 #open the page
-raw_page = request.urlopen(page_url)
+soup = get_url(page_url)
 
-#create object from the page
-soup = BeautifulSoup(raw_page, 'html5lib')
-
+print(soup)
 
 
 # general data cleaning, manipulation and aggregation scripts here 
