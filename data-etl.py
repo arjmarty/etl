ba34ead221a8de0a432766abf02dc9d6907de6a4
arjmarty <annrose0227@gmail.com>
2025-06-29 23:55:12 +0800

    modify etl scripts

--- data-etl.py
+++ data-etl.py
@@ -8,10 +8,16 @@ import json
 from bs4 import BeautifulSoup
 from urllib import request
 import time
+from selenium import webdriver
 
+
+# Set up Chrome options
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')  # Run in headless mode
+driver = webdriver.Chrome(options=options)
 
 # url of intended website
-page_url1 = "https://cars.mitula.ph/searchC/level1-Camarines+Sur/sort-0/q-naga-city-bicol?req_sgmt=REVTS1RPUDtTRU87U0VSUDs="
+page_url1 = "http://www.amlc.gov.ph/images/PDFs/Main/SEC31Jan25.pdf"
 page_url2 = "https://checkwithsec.sec.gov.ph/check-with-sec/suspended"
 
 response1 = requests.get(page_url1)
@@ -23,10 +29,11 @@ soup2 = BeautifulSoup(response2.content, 'html5lib')
 text_only1 = soup1.text
 text_only2 = soup2.text
 
+# print(text_only1)
 print(text_only1)
-print(soup2)
 
 
 # general data cleaning, manipulation and aggregation scripts here 
-data = pd.DataFrame(text_only1)
+data = pd.DataFrame(text_only1) 
 num = data.count().sort_index(ascending=False).tail()
+pctchange = ((curr - prev) / prev) * 100
